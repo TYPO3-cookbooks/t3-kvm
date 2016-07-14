e76212ac8d4505c9593441b4cd77169f3799b83a
Steffen Gebert <steffen.gebert@typo3.org>
2016-07-14 19:36:47 +0200

    [TASK] Pin sysctl cookbook to 0.7.0
    Otherwise, we run in the following dependency conflict
    (e.g. in site-proxytypo3org)
    
    > - `ohai (~> 3.0)` required by `sysctl-0.7.5`
    > - `ohai (< 4.0.0)` required by `t3-openvz-1.1.3`
    > - `ohai (~> 2.0)` required by `nginx-2.7.6`
    > Unable to find a solution for demands: site-proxytypo3org (1.0.12)
    
    Therefore, enforce a version of sysctl that depends on ohai ~> 2.0.
    
    Can be removed, if nginx > 2.7.6 depends on a newer ohai version.

--- metadata.rb
+++ metadata.rb
@@ -6,3 +6,8 @@ description      'Wrapper for the KVM cookbook'
 version          '0.1.0'
 
 depends 'kvm', '= 0.4.0'
+
+# We manually downgrade this so that it depends on ohai~>2.0.
+# Otherwise we have a conflict with nginx 2.x cookbook.
+# This is visible in site-proxytypo3org
+depends 'sysctl', '= 0.7.0'
