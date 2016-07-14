f97aa71b4f7af885da23a8adefde5b9915272cec
Steffen Gebert <steffen.gebert@typo3.org>
2016-07-14 19:58:35 +0200

    [TASK] Bump version to 1.0.1

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,7 @@ maintainer       'Andri Steiner'
 maintainer_email 'andri.steiner@typo3.org'
 license          'Apache 2.0'
 description      'Wrapper for the KVM cookbook'
-version          '0.1.0'
+version          '0.1.1'
 
 depends 'kvm', '= 0.4.0'
 
