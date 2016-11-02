409ecb578bad2c38dc92daf37f8aa483983339c1
Michael Stucki <michael.stucki@typo3.org>
2016-11-02 12:43:21 +0100

    [TASK] Bump versoin to 0.2.1

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,7 @@ maintainer       'Andri Steiner'
 maintainer_email 'andri.steiner@typo3.org'
 license          'Apache 2.0'
 description      'Wrapper for the KVM cookbook'
-version          '0.2.0'
+version          '0.2.1'
 
 depends 'kvm', '= 0.4.0'
 
